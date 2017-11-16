c48eef7f27f077ca7cea83e9f4841e2c7ca47028
default <el16hes@eentpc243.leeds.ac.uk>
2017-11-16 12:35:06 +0000

    Created a data model

---
+++ MealPlanApp/DataModel.m
@@ -0,0 +1,13 @@
+//
+//  DataModel.m
+//  MealPlanApp
+//
+//  Created by Helen Simmonds [el16hes] on 16/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import "DataModel.h"
+
+@implementation DataModel
+
+@end
