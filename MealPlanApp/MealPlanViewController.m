42360a5cad835896846293342aaac64f6a67b697
default <el16hes@eentpc243.leeds.ac.uk>
2017-12-05 17:57:21 +0000

    Changed the meal plan view to get rid of 'drop-downs'

---
+++ MealPlanApp/MealPlanViewController.m
@@ -0,0 +1,32 @@
+//
+//  MealPlanViewController.m
+//  MealPlanApp
+//
+//  Created by Helen Simmonds [el16hes] on 05/12/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import "MealPlanViewController.h"
+
+@interface MealPlanViewController ()
+
+@end
+
+@implementation MealPlanViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+
+
+
+@end
