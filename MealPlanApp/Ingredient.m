84cb47bd7dfee21851212cd76f9da7b41f0e3d59
Helen Simmonds [el16hes] <el16hes@eentpc129.leeds.ac.uk>
2017-12-06 10:32:48 +0000

    Initial Commit

---
+++ MealPlanApp/Ingredient.m
@@ -0,0 +1,13 @@
+//
+//  Ingredient.m
+//  MealPlanApp
+//
+//  Created by Helen Simmonds [el16hes] on 16/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import "Ingredient.h"
+
+@implementation Ingredient
+
+@end
